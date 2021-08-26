522d6dd33aed1ed0d3adcb460223cd040e7520da
Aniruddha <aniruddha200baidya@protonmail.com>
2021-08-26 15:08:28 +0100

    Initial Commit

---
+++ Sunday Assignment/Sunday_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sunday_AssignmentApp.swift
+//  Sunday Assignment
+//
+//  Created by administrator on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Sunday_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
